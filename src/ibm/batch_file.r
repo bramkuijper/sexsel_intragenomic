286547efaceb7a8f3993c565c49f18b520671897
megabyte22 <a.l.w.kuijper@exeter.ac.uk>
2020-12-16 22:13:26 +0000

    changed batch to accommodate that nf = K - nm

--- src/ibm/batch_file.r
+++ src/ibm/batch_file.r
@@ -3,7 +3,6 @@ library("simulation.utils")
 params <- list()
 
 params$nm <- seq(1,10,1)
-params$nf <- seq(1,10,1)
 params$init_t <- 0.01
 params$init_p <- 0.2
 params$init_tprime <- 0.01
@@ -35,7 +34,13 @@ all_params = as.data.frame(expand.grid(params))
 
 all_params["n_patches"] <- 8000 / all_params$nf  
 
-setwd("/Users/bram/Projects/sexsel_intragenomic/src/ibm" )
+# rather than vary nf and nm independently, we 
+# focus on a scenario where nf is high and nm low
+# and vice versa. Hence, if nm = 10, nf = 1
+# if nm = 1, nf = 10 and all values in between:
+all_params["nf"] <- 11 - all_params["nm"]
+
+#setwd("/Users/bram/Projects/sexsel_intragenomic/src/ibm" )
 
 # make the batch file
 make.batch.file.yaml(
